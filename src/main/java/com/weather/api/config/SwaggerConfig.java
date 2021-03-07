22311b5fc5d300f1341a24b51ff1b150495635f8
raghunadhpokkalath <raghunadhp@gmail.com>
2021-03-08 10:38:25 +1100

    updated swagger specs

--- src/main/java/com/weather/api/config/SwaggerConfig.java
+++ src/main/java/com/weather/api/config/SwaggerConfig.java
@@ -6,16 +6,14 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@EnableSwagger2WebMvc
 public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).
                 select().
-                apis(RequestHandlerSelectors.basePackage("com.weather.api.controller")).
+                apis(RequestHandlerSelectors.basePackage("com.weather.api")).
                 paths(PathSelectors.any()).
                 build();
     }
--- src/main/java/com/weather/api/controller/WeatherController.java
+++ src/main/java/com/weather/api/controller/WeatherController.java
@@ -2,10 +2,7 @@ package com.weather.api.controller;
 
 import com.weather.api.dto.WeatherResponse;
 import com.weather.api.service.WeatherService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,14 +21,14 @@ public class WeatherController {
     WeatherService weatherService;
 
     @GetMapping("/data/description")
-    @ApiOperation(value = "Retrieve Weather Description", response = WeatherResponse.class, notes = "This endpoint retrieve Weather Description based on Country cide and city code ")
+    @ApiOperation(value = "Retrieve Weather Description", response = WeatherResponse.class, notes = "This endpoint retrieve Weather Description based on Country And City")
     @ApiResponses({ @ApiResponse(code = 200, message = "Weather Description"),
-            @ApiResponse(code = 400, message = "Invalid Request"),
+            @ApiResponse(code = 400, message = "Invalid Request Parameters"),
             @ApiResponse(code = 500, message = "Internal Server Error") })
 
     public WeatherResponse getWeatherDescription(
-            @RequestParam @NotEmpty(message = "Country  is Mandatory") String county, @RequestParam @NotEmpty(message = "City  is Mandatory") String city,
-            @RequestHeader(name = "x-api-key") String apiKey) {
+            @ApiParam("Country Name") @RequestParam @NotEmpty(message = "Country  is Mandatory")  String county, @ApiParam("City Name") @RequestParam @NotEmpty(message = "City  is Mandatory") String city,
+           @ApiParam("API Key")  @RequestHeader(name = "x-api-key") String apiKey) {
         return weatherService.getData(county, city, apiKey);
 //                map(weatherResponse -> ResponseEntity.ok(weatherResponse))
 //                .defaultIfEmpty(ResponseEntity.notFound().build());
