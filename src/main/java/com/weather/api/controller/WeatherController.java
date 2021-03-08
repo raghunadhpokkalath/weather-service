0ce8465224c152fcf5d2487c2e38706c8542298f
raghunadhpokkalath <raghunadhp@gmail.com>
2021-03-08 13:18:27 +1100

    bucket limit

--- src/main/java/com/weather/api/controller/WeatherController.java
+++ src/main/java/com/weather/api/controller/WeatherController.java
@@ -2,8 +2,14 @@ package com.weather.api.controller;
 
 import com.weather.api.dto.WeatherResponse;
 import com.weather.api.service.WeatherService;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -11,14 +17,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotEmpty;
+import java.time.Duration;
 
 @Api(value = "Weather Data  Retrieval")
 @RestController
 @Validated
 public class WeatherController {
 
+    private final Bucket bucket;
     @Autowired
     WeatherService weatherService;
+
+    public WeatherController() {
+        Bandwidth limit = Bandwidth.classic(2, Refill.greedy(2, Duration.ofMinutes(1)));
+        this.bucket = Bucket4j.builder()
+                .addLimit(limit)
+                .build();
+
+
+    }
 
     @GetMapping("/data/description")
     @ApiOperation(value = "Retrieve Weather Description", response = WeatherResponse.class, notes = "This endpoint retrieve Weather Description based on Country And City")
@@ -26,12 +43,14 @@ public class WeatherController {
             @ApiResponse(code = 400, message = "Invalid Request Parameters"),
             @ApiResponse(code = 500, message = "Internal Server Error")})
 
-    public WeatherResponse getWeatherDescription(
+    public ResponseEntity<WeatherResponse> getWeatherDescription(
             @ApiParam("Country Name") @RequestParam @NotEmpty(message = "Country  is Mandatory") String county, @ApiParam("City Name") @RequestParam @NotEmpty(message = "City  is Mandatory") String city,
             @ApiParam("API Key") @RequestHeader(name = "x-api-key") String apiKey) {
-        return weatherService.getData(county, city, apiKey);
-//                map(weatherResponse -> ResponseEntity.ok(weatherResponse))
-//                .defaultIfEmpty(ResponseEntity.notFound().build());
+        if (bucket.tryConsume(1)) {
+            return ResponseEntity.ok(weatherService.getData(county, city, apiKey));
+        }
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+
     }
 
 }
